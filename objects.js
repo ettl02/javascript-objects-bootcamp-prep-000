8ea242f4d0da70e1db4cfc20e5ccbdf686646923
Emily Wessel <ettl02@gmail.com>
2018-05-18 13:56:58 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 var playlist = {
   'My Bloody Valentine': 'Slowdive'
   
@@ -14,3 +15,18 @@ function removeFromPlaylist(playlist, artist) {
 
   return playlist
 }
+=======
+var playlist = { Slowdive: 'My Bloody Valentine" };
+
+function updatePlaylist (playlist, artistName, songTitle) {
+  var playlist = { [artistName]: songTitle };
+  return playlist;
+}
+
+function removeFromPlaylist(playlist, artistName) {
+   var songTitle = "song";
+   var playlist = { [artistName]: songTitle };
+  delete playlist[artistName];
+    return playlist;
+}
+>>>>>>> 102be49f2939d21663ea12935733f6e0da99b333
